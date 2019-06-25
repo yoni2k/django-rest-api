a9b80f2ac77d79e83eadc9f77031ce3b303c9959
yoni2k <yoni.k81@gmail.com>
2019-06-25 12:22:27 +0300

    Adding synchronizer and post() function to get input from user, validate it and return a 200 response when valid and 400 with specific error when not valid

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status #list of handy http status codes
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """ Test API View of Hello World """
+    #configures API view with specific serializer
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """ Returns a list of APIView features """
@@ -16,3 +20,20 @@ class HelloApiView(APIView):
         # Response must contain a dictionary or list of what to return, will be converted to JSON
         return Response({'message' : 'Hello!',
                         'an_apiview' : an_apiview})
+
+
+    def post(self, request, format=None):
+        """ Create a hello message with our name """
+        serializer = self.serializer_class(data=request.data)
+
+        #in our case is_valid checks that the length is <= 10
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            #serializer created the errors list of parsing
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST #by default Response returns 200
+            )
