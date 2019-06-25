2cd636cf0c1ec930cd90afc6dfbb0cccb695a328
yoni2k <yoni.k81@gmail.com>
2019-06-25 13:24:35 +0300

    Adding rest of the ViewSet methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -58,6 +58,9 @@ class HelloApiView(APIView):
 
 class HelloViewSet(viewsets.ViewSet):
     """ Test API ViewSet """
+    #reusing the same serializer
+    serializer_class = serializers.HelloSerializer
+
 
     def list(self, request):
         """ List all objects """
@@ -67,3 +70,35 @@ class HelloViewSet(viewsets.ViewSet):
             'Provides more functionality wiht less code',
         ]
         return Response({'message':'Hello!', 'a_viewset' : a_viewset})
+
+    def create(self, request):
+        """ Create a new hello message """
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message', message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """ Handle getting an object by it's ID """
+        return Response({'http_method' : 'GET'})
+
+
+    def update(self, request, pk=None):
+        """ Handle updating an object by it's ID """
+        return Response({'http_method' : 'PUT'})
+
+
+    def partial_update(self, request, pk=None):
+        """ Handle updating part an object by it's ID """
+        return Response({'http_method' : 'PATCH'})
+
+
+    def destroy(self, request, pk=None):
+        """ Handle destoying an object by it's ID """
+        return Response({'http_method' : 'DELETE'})
